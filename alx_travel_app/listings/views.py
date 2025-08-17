033342dd978fb001712d93279fe6b15016efa1a5
Benareyo <betelyohannes39@gmail.com>
2025-08-16 23:15:43 -0700

    Initialize 0x02 project from 0x01

---
+++ alx_travel_app/listings/views.py
@@ -0,0 +1,19 @@
+# listings/views.py
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class SampleAPIView(APIView):
+    def get(self, request):
+        return Response({"message": "Welcome to ALX Travel App"})
